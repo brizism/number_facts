59d6ca9b513154cb54171be3ee4120aaf3c0dfbb
brizism <montielbriseida@gmail.com>
2018-03-05 13:03:08 -0500

    fetch numbers from dom and add event handlers to fetch data

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,18 @@
-const numbers = document.querySelectorAll('.grid__item');
+// Init numbers
+const numbers = new Numbers();
 
-numbers.forEach(number => {
+const allNumbers = document.querySelectorAll('.grid__item');
+
+allNumbers.forEach(number => {
   let randomNum = Math.floor((Math.random() * 100) + 1)
   number.innerHTML = randomNum;
-  
+
+  number.addEventListener('click', () => {
+    numbers.getNumbers(randomNum)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  })
 })
+
+
+
