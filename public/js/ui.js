7fc516acf764415542caaec9cb812d92786e7e8f
brizism <montielbriseida@gmail.com>
2018-03-09 14:10:14 -0500

    fetch dom elems

---
+++ public/js/ui.js
@@ -0,0 +1,11 @@
+class UI {
+  constructor(){
+    this.numbers = document.getElementById('numbers-content');
+    this.modal = document.getElementById('numbers-modal');
+  }
+
+  // Display number facts in UI
+  paintFacts(numbers){
+    console.log(numbers);
+  }
+}
