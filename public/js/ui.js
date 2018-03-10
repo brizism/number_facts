447a17c446420e7991f1e2fe5cbe36c6bc58a9ce
brizism <montielbriseida@gmail.com>
2018-03-09 22:55:53 -0500

    paint fetched data

--- public/js/ui.js
+++ public/js/ui.js
@@ -7,5 +7,17 @@ class UI {
   // Display number facts in UI
   paintFacts(numbers){
     console.log(numbers);
+    this.modal.style.display = 'block';
+    let output = `
+      <div class="numbers-modal__wrapper">
+        <span id="close">&times;</span>
+        <h1 class="numbers-modal__number">${numbers.slice(0,2)}</h1>
+        <div className="numbers-modal__fact">
+          <p>${numbers.slice(3)}</p>
+        </div>
+        <h3 class="numbers-modal__bgnumber">${numbers.slice(0,2)}</h3>
+      </div>
+    `
+    this.numbers.innerHTML = output;
   }
 }
