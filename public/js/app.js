faeada5985b489d6956ed237b454be5f16f473c3
brizism <montielbriseida@gmail.com>
2018-03-13 01:11:19 -0400

    create funcs to generate random and get number

--- public/js/app.js
+++ public/js/app.js
@@ -4,18 +4,31 @@ const numbers = new Numbers();
 // Init ui
 const ui = new UI();
 
-const allNumbers = document.querySelectorAll('.number h1');
+const allNumbers = document.querySelectorAll('.action');
+const shuffle = document.getElementById('shuffle');
 
-allNumbers.forEach(number => {
-  let randomNum = Math.floor((Math.random() * 100) + 1)
-  number.innerHTML = randomNum;
+function generateRandom() {
+  allNumbers.forEach(number => {
+    let randomNum = Math.floor((Math.random() * 100) + 1)
+    number.innerHTML = randomNum;
 
-  number.addEventListener('click', () => {
-    numbers.getNumbers(randomNum)
+  })
+}
+
+function getNumber() {
+  allNumbers.forEach(number => {
+    number.addEventListener('click', () => {
+      console.log(number.innerHTML);
+      numbers.getNumbers(number.innerHTML)
       .then(res => ui.paintFacts(res))
       .catch(err => console.log(err))
+    })
   })
+}
+
+shuffle.addEventListener('click', () => {
+  generateRandom()
 })
 
-
-
+getNumber()
+generateRandom()
