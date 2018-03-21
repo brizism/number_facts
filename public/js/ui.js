1ffd20ef6e627c078e7827ad94f35195bdf9b6fa
brizism <montielbriseida@gmail.com>
2018-03-21 18:54:56 -0400

    fix 3 digit number display

--- public/js/ui.js
+++ public/js/ui.js
@@ -6,17 +6,17 @@ class UI {
 
   // Display number facts in UI
   paintFacts(numbers){
-    console.log(numbers);
+    console.log(typeof(numbers))
     this.modal.style.display = 'block';
     let output = `
       <div class="numbers-modal__wrapper">
         <span id="close">&times;</span>
-        <h1 class="numbers-modal__number">${numbers.slice(0,2)}</h1>
+        <h1 class="numbers-modal__number">${numbers.charAt(2) == 0 ? numbers.slice(0,3) : numbers.slice(0,2)}</h1>
         <div class="numbers-modal__fact">
           <p>${numbers}</p>
         </div>
         <div class="numbers-modal__bgnumber">
-          <h3>${numbers.slice(0,2)}</h3>
+          <h3>${numbers.charAt(2) == 0 ? numbers.slice(0,3) : numbers.slice(0,2)}</h3>
         </div>
       </div>
     `
