569d7aee3638a210efcf79cbddde8cb0e0f6367c
brizism <montielbriseida@gmail.com>
2018-03-21 20:59:49 -0400

    on submit fetch by year

--- public/js/app.js
+++ public/js/app.js
@@ -6,21 +6,21 @@ const ui = new UI();
 
 const allNumbers = document.querySelectorAll('.action');
 const shuffle = document.getElementById('shuffle');
+const searchForm = document.getElementById('search-num');
+const numberInput = document.getElementById('search-input');
 
 function generateRandom() {
   allNumbers.forEach(number => {
-    let randomNum = Math.floor((Math.random() * 100) + 1)
+    let randomNum = Math.floor((Math.random() * 100) + 1);
     number.innerHTML = randomNum;
-
   })
 }
 
 function getNumber() {
   allNumbers.forEach(number => {
     number.addEventListener('click', () => {
-      console.log(number.innerHTML);
       numbers.getNumbers(number.innerHTML)
-      .then(res => ui.paintFacts(res))
+      .then(res => ui.paintFacts(res.numbers, number.innerHTML))
       .catch(err => console.log(err))
     })
   })
@@ -29,6 +29,24 @@ function getNumber() {
 shuffle.addEventListener('click', () => {
   generateRandom()
 })
+
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const number = numberInput.value;
+  
+  if(number !== ''){
+    numbers.getNumbers(number)
+      .then(res => ui.paintYearFacts(res.numbersYear, number))
+      .catch(err => console.log(err))
+  } else {
+    ui.showMessage('Looks like you did not enter a number 😬');
+  }
+
+  // Clear input
+  numberInput.value = '';
+})
+
 
 getNumber()
 generateRandom()
